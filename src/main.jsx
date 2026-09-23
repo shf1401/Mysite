d4922e02ee37a650adab735991dab796de8cc6e5
sara <sarahashemi.f@gmail.com>
2026-09-23 13:23:48 +0330

    edit app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 // پیدا کردن المان اصلی صفحه (سازگار با هر دو شناسه root یا app)
